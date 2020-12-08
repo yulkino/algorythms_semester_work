6ead2944bac3b7489ff6ac2f1093aa8c91b844af
yulkino <yulkino@hotmail.com>
2020-12-08 21:14:09 +0500

    Добавьте файлы проекта.

---
+++ algorythms_semester_work/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace algorythms_semester_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
